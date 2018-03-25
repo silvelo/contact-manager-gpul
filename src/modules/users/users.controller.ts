5c789132f5a6b54216b0196f50a83b5e8729d8d9
Silvelo <arturo.silvelo@gmail.com>
2018-03-25 19:25:24 +0200

    Add methods to controller and service

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,32 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Delete, Post, Param, Body } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { IUser } from "./users.model";
 
 @Controller('users')
 export class UsersController {
 
     constructor(private usersService: UsersService) { }
 
+
+    @Post()
+    createUser(@Body() newUser: IUser) {
+        return this.usersService.createUser(newUser);
+    }
+
     @Get()
-    retrieveAll() {
+    retrieveAll(): IUser[] {
         return this.usersService.retreiveAll()
     }
+
+
+    @Get(':id')
+    retreiveById(@Param('id') id: string): IUser {
+        return this.usersService.retrieveById(+id);
+    }
+
+    @Delete(':id')
+    removeById(@Param('id') id: string) {
+        return this.usersService.removeById(+id);
+    }
+
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,10 +1,28 @@
 import { Component } from "@nestjs/common";
+import { IUser } from "./users.model";
 
 @Component()
 export class UsersService {
-    private users = [{ name: 'Arturo' }, { name: 'Pepe' }];
+    private users: IUser[] = [{ id: 1, name: 'Arturo', number: '555-5555' }, { id: 2, name: 'Pepe', number: '5555-6666' }];
+    private positionId: number = 2;
 
-    retreiveAll() {
+    retreiveAll(): IUser[] {
         return this.users;
     }
+
+    retrieveById(userId: number) {
+        return this.users.find(user => user.id === userId);
+    }
+
+    createUser(newUser: IUser) {
+        this.positionId++;
+        this.users.push({ ...newUser, id: this.positionId });
+    }
+
+    removeById(userId: number) {
+        let index = this.users.findIndex(user => user.id == userId);
+        this.users.splice(index, 1);
+    }
+
+
 }
